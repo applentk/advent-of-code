e50ee685228ea61f4a265b74571b184282643918
applentk <tanoobguy12@gmail.com>
2025-12-01 19:09:35 +0700

    Add 2025

---
+++ 2025/READMD.md
@@ -0,0 +1,16 @@
+# Advent of code 2025
+This year I've decide to use AoC to practice rust, enjoy :D
+
+## Run the solution
+If you tryna run, first you have to put input into `src/inputs` with
+the number of day first, for example
+
+```sh
+src/inputs/day1.in
+```
+
+then simply run it by
+
+```sh
+cargo run --bin day1
+```
---
+++ 2025/src/main.rs
@@ -0,0 +1,22 @@
+// Using this as template
+use std::{fs, io};
+
+fn part_1(inputs: &Vec<&str>) {
+
+}
+
+fn part_2(inputs: &Vec<&str>) {
+   
+}
+
+fn main() -> io::Result<()> {
+    let file_path = "src/inputs/day.in";
+    let input = fs::read_to_string(&file_path)?;
+
+    let inputs: Vec<&str> = input.split("\n").collect();
+
+    part_1(&inputs);
+    part_2(&inputs);
+
+    Ok(())
+}
